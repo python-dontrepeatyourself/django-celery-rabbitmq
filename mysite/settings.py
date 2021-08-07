83cfffae71a7bbe25d0156c71eb922d2cc26a75a
rouizi <cinorouizi@gail.com>
2021-08-07 08:33:17 +0200

    integrating celery with django

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'amqp://localhost'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alfabravo318@gmail.com' # put your gmail address here
+EMAIL_HOST_PASSWORD = '<your-gmail-password>'
+EMAIL_USE_TLS = True
